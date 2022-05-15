af9cdb159779937a58aac0b6842950629e22faa0
RIZVANSHERIFFGA <103945205+RIZVANSHERIFFGA@users.noreply.github.com>
2022-05-15 14:09:57 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+
+
+public class Factorial {
+
+	 public static void main(String[] args) {
+
+			// Declare your input as 5
+			int a;
+					
+		  for (a = 5; a<5; a++) 
+		  {
+
+			  for (int j = 0; j < 5;j++ )
+			  {
+			  System.out.println(" "+j+" ");
+			
+		}
+			  System.out.println(" ");
+		 
+		  }
+			
+	 }
+}
---
+++ Mobile.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Mobile {
+	
+
+	
+	
+	
+
+	public static void main(String[] args) {
+	
+		int i = 1;
+	for ( i = 1; i<=100; i++);
+	System.out.println(i);
+	
+
+}
+}
